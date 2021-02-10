d37eca77ff7710a6dd8201b6e1f487f0ef6b3f5d
Salvatore Napoli <snapoli1998@gmail.com>
2021-02-10 12:47:53 +0100

    Milestone 2: serie TV

--- js/script.js
+++ js/script.js
@@ -4,18 +4,25 @@ new Vue ({
 	el: '#boolflix',
 
 	data: {
-		searchedMovie: '',
-		movies: ''
+		searchedEntity: '',
+		movies: '',
+		tvSeries: ''
 	},
 
 	methods: {
-		searchMovie: function () {
+		searchEntity: function () {
 			const self = this;
 
 			axios
-				.get(`https://api.themoviedb.org/3/search/movie/?api_key=a12efa1af0ae84b6f6e32a9c870485be&language=it-IT&query=${this.searchedMovie}`)
+				.get(`https://api.themoviedb.org/3/search/movie/?api_key=a12efa1af0ae84b6f6e32a9c870485be&language=it-IT&query=${this.searchedEntity}`)
 				.then(function (movie) {
 					self.movies = movie.data.results;
+				});
+
+			axios
+				.get(`https://api.themoviedb.org/3/search/tv?api_key=a12efa1af0ae84b6f6e32a9c870485be&language=it_IT&query=${this.searchedEntity}`)
+				.then(function (series) {
+					self.tvSeries = series.data.results;
 				});
 		}
 	}
