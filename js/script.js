3b67ab3580f3679dddfb82c030b79414e94575b6
Salvatore Napoli <snapoli1998@gmail.com>
2021-02-11 23:07:43 +0100

    Refactoring funzioni

--- js/script.js
+++ js/script.js
@@ -4,26 +4,31 @@ new Vue ({
 	el: '#boolflix',
 
 	data: {
-		searchedEntity: '',
-		movies: '',
-		tvSeries: ''
+		searchedMedia: '',
+		media: []
 	},
 
 	methods: {
-		searchEntity: function () {
-			const self = this;
+		searchMedia () {
+			this.media = [];
+			this.searchCategory('https://api.themoviedb.org/3/search/movie');
+			this.searchCategory('https://api.themoviedb.org/3/search/tv');
+		},
 
+		searchCategory (apiUrl)	{
 			axios
-				.get(`https://api.themoviedb.org/3/search/movie/?api_key=a12efa1af0ae84b6f6e32a9c870485be&language=it-IT&query=${this.searchedEntity}`)
-				.then(function (movie) {
-					self.movies = movie.data.results;
+				.get(`${apiUrl}?api_key=a12efa1af0ae84b6f6e32a9c870485be&language=it-IT&query=${this.searchedMedia}`)
+				.then((element) => {
+					this.media = [...this.media,...element.data.results];
 				});
+		},
 
-			axios
-				.get(`https://api.themoviedb.org/3/search/tv?api_key=a12efa1af0ae84b6f6e32a9c870485be&language=it_IT&query=${this.searchedEntity}`)
-				.then(function (series) {
-					self.tvSeries = series.data.results;
-				});
+		setPoster (path) {
+			if (path !== null) {
+				return `url(https://image.tmdb.org/t/p/w500/${path})`;
+			} else {
+				return 'url(https://www.killthebeat.it/wp-content/uploads/2020/01/bianco-punto-interrogativo-su-uno-sfondo-circolare-nero_318-35996.jpg)';
+			}
 		}
 	}
 });
